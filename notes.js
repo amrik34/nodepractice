91bef342727310212237fe2d9bdc8af1b2fe9d12
amrik34 <amrik3412singh@gmail.com>
2025-07-17 10:36:00 +0530

    graphql-code

--- notes.js
+++
@@ -1,7 +0,0 @@
-console.log("notes avialbale");
-var age = 24;
-var add = (a, b) => a + b;
-module.exports = {
-  age,
-  add,
-};
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require("apollo-server");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => "Hello world testing",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
